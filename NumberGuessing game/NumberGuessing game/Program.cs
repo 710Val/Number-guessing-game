4dc36d30a7a119972e554238750638f093854ff5
710Val <86447935+710Val@users.noreply.github.com>
2022-04-04 17:02:23 -0300

    Add files via upload

---
+++ NumberGuessing game/NumberGuessing game/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+namespace NumberGuesser
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Valzinha";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+          
+            Console.ResetColor();
+
+            Console.WriteLine("What is your name?");
+
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game!", inputName);
+
+            while (true)
+            {
+
+                // Criar um numero randomico
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+
+
+                int guess = 0;
+
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out guess))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Hey! Thats not a number, please enter an actual number");
+
+                        Console.ResetColor();
+                        continue;
+                    }
+
+
+
+                    guess = Int32.Parse(input);
+
+                    if (guess != correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong number, please try again!");
+
+                        Console.ResetColor();
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine("You are CORRECT!");
+
+                Console.ResetColor();
+
+                Console.WriteLine("Play Again?[Y or N]");
+                string answer = Console.ReadLine().ToUpper();
+               
+                if(answer != answer)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please type Y or N!!");
+                    Console.ResetColor();
+
+
+                }
+                if(answer == "Y")
+                {
+                    continue;
+                }
+                else if (answer =="N")
+                {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+
+            }
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
